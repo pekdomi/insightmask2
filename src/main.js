89590ccf0f8b9a7eb8c92c33ba66930de972c2bd
M-dev-art <domonkos314@gmail.com>
2023-09-29 04:00:59 +0200

    deploy

--- src/main.js
+++ src/main.js
@@ -4,34 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-      {
-        path: "/",
-        name: "login",
-        component: LoginView
-      },
-      {
-          path: "/company",
-          name: "company",
-          component: CeoView
-      },
-      
-      {
-          path: "/user",
-          name: "user",
-          component: NpcView
-      },
-      {
-        path: '/:pathMatch(.*)*',
-        name: 'not-found',
-        component: LoginView
-      }
-  ]
-  })
+import router from './router'
 
 const app = createApp(App)
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,29 @@
-
+import { createRouter, createWebHashHistory } from 'vue-router'
 import CeoView from "../views/CeoView.vue"
 import LoginView from "../views/LoginView.vue"
 import NpcView from "../views/NpcView.vue"
 
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "login",
+      component: LoginView
+    },
+    {
+        path: "/company",
+        name: "company",
+        component: CeoView
+    },
+    
+    {
+        path: "/user",
+        name: "user",
+        component: NpcView
+    }
+    
+]
+})
 
+export default router
