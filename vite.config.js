1152a544cbce746ecdbf5ae9437a7bb3fd0a72f1
M-dev-art <domonkos314@gmail.com>
2023-09-29 03:29:26 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pekdomi.github.io/',
+  base: '/insightmask/',
   plugins: [
     vue(),
   ],
