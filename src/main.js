31e051ee954404d0a2914e324b5ff5429a321f5b
M-dev-art <domonkos314@gmail.com>
2023-09-29 03:52:15 +0200

    deploy

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(),
     routes: [
       {
         path: "/",
